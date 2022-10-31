abaddae9178b68ed89f33ec60c9de6e8946aa45d
AndersonLucas30 <andersoncui3.0@gmail.com>
2022-10-31 13:50:40 -0300

    a

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'yoursecret',
-    database: 'mongodb+srv://anderson:anderson03%40jardim@cluster0.8fpaokk.mongodb.net/cadsatro_novo_usuario?retryWrites=true&w=majority'
+    database: 'mongodb+srv://anderson:anderson03@jardim@cluster0.8fpaokk.mongodb.net/cadsatro_novo_usuario?retryWrites=true&w=majority'
 }
